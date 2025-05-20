81e820ab49307feff7480b5f98343f4197dd0838
AmeyaMad <ameya.mad@gmail.com>
2025-05-20 03:27:17 -0700

    Changed color scheme

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,6 @@ const PORT = 3000;
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://ameya.software`);
+    console.log(`Server running at https://ameya.software`);
 
 });
